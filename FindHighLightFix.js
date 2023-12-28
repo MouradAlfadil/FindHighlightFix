0fd93581f25701e9e504351cf0cbec0d43ac88ac
MouradAlfadil <119465416+MouradAlfadil@users.noreply.github.com>
2023-12-28 01:12:50 -0500

    Update FindHighLightFix.js

--- FindHighLightFix.js
+++ FindHighLightFix.js
@@ -1,9 +1,15 @@
-document.addEventListener('find', function(event) {
-  var foundText = event.target.toString().trim();
-  if (foundText !== '') {
-    var foundElements = document.querySelectorAll(':contains(' + foundText + ')');
+// Function to apply styles to found text
+function applyStylesToFoundText() {
+  var selectedText = window.getSelection().toString().trim();
+  if (selectedText !== '') {
+    var foundElements = document.querySelectorAll(':contains(' + selectedText + ')');
     foundElements.forEach(function(element) {
       element.classList.add('found-text');
     });
   }
+}
+
+// Call the function when the user interacts with your custom search element
+document.getElementById('your-search-button').addEventListener('click', function() {
+  applyStylesToFoundText();
 });
