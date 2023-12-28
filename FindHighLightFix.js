6dc715958b6b8f956722286ca0962159092bab51
MouradAlfadil <119465416+MouradAlfadil@users.noreply.github.com>
2023-12-28 01:13:48 -0500

    Update FindHighLightFix.js

--- FindHighLightFix.js
+++ FindHighLightFix.js
@@ -1,15 +1,15 @@
-// Function to apply styles to found text
-function applyStylesToFoundText() {
+// Function to adjust the text size of found text
+function adjustTextSizeOfFoundText() {
   var selectedText = window.getSelection().toString().trim();
   if (selectedText !== '') {
     var foundElements = document.querySelectorAll(':contains(' + selectedText + ')');
     foundElements.forEach(function(element) {
-      element.classList.add('found-text');
+      element.style.fontSize = '18px'; // Adjust the font size as needed
     });
   }
 }
 
 // Call the function when the user interacts with your custom search element
 document.getElementById('your-search-button').addEventListener('click', function() {
-  applyStylesToFoundText();
+  adjustTextSizeOfFoundText();
 });
