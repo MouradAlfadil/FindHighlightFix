041ab92f410d094509da5cd8fe82eca86e590619
MouradAlfadil <119465416+MouradAlfadil@users.noreply.github.com>
2023-12-28 01:17:31 -0500

    Update FindHighLightFix.js

--- FindHighLightFix.js
+++ FindHighLightFix.js
@@ -1,15 +1,14 @@
-// Function to adjust the text size of found text
-function adjustTextSizeOfFoundText() {
-  var selectedText = window.getSelection().toString().trim();
-  if (selectedText !== '') {
-    var foundElements = document.querySelectorAll(':contains(' + selectedText + ')');
-    foundElements.forEach(function(element) {
-      element.style.fontSize = '180px'; // Adjust the font size as needed
-    });
-  }
+// Function to set ::selection styles dynamically
+function setSelectionStyles() {
+  // Fetch colors from GitHub repository (replace with your actual endpoint)
+  fetch('https://raw.githubusercontent.com/MouradAlfadil/FindHighlightFix/main/colors.json')
+    .then(response => response.json())
+    .then(data => {
+      // Apply styles using the fetched colors
+      document.styleSheets[0].insertRule(`::selection { background-color: ${data.backgroundColor} !important; color: ${data.textColor}; }`, 0);
+    })
+    .catch(error => console.error('Error fetching colors:', error));
 }
 
-// Call the function when the user interacts with your custom search element
-document.getElementById('your-search-button').addEventListener('click', function() {
-  adjustTextSizeOfFoundText();
-});
+// Call the function when the page loads
+window.addEventListener('load', setSelectionStyles);
